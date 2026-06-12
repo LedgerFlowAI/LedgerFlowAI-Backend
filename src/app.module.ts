a23d408df3462d26c23faed84b1e20af3d3687be
Olawale880 <ibrahimolawale880@gmail.com>
2026-06-12 14:54:45 +0100

    all is ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { FinanceModule } from './finance/finance.module';
+import { ReputationModule } from './reputation/reputation.module';
+import { AiModule } from './ai/ai.module';
+import { PayrollModule } from './payroll/payroll.module';
+import { ExpensesModule } from './expenses/expenses.module';
+import { PaymentsModule } from './payments/payments.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { WalletModule } from './wallet/wallet.module';
+import { OrganisationsModule } from './organisations/organisations.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, FinanceModule, ReputationModule, AiModule, PayrollModule, ExpensesModule, PaymentsModule, InvoiceModule, WalletModule, OrganisationsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
